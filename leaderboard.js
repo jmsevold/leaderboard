7479f3c99823017a09ac2b9298f16a8e4be522eb
Jonathan Sevold <jonathansevold@Jonathans-MacBook-Pro.local>
2015-04-13 06:38:22 -0700

    advanced operators part 2

--- leaderboard.js
+++ leaderboard.js
@@ -9,7 +9,31 @@ if (Meteor.isClient){
 
     otherFunction: function(){
       return 'other function'
+    },
+
+    selectedClass: function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if (playerId == selectedPlayer) {
+        return 'selected'
+      }
     }
+
+  });
+
+
+  Template.leaderboard.events({
+
+    'click .player': function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    },
+
+    'click .increment': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      console.log(selectedPlayer);
+}
+
   });
 };
 
